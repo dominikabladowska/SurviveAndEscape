69f0d8681a4b7108706c7511fada55aff83a12c5
dominikabladowska <dominikabladowska@gmail.com>
2022-06-25 16:50:56 +0200

    Init

---
+++ Source/SurviveAndEscape/SurviveAndEscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurviveAndEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVEANDESCAPE_API ASurviveAndEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
